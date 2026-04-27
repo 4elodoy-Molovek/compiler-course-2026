4028d44dbbfe827a86b7306084ae6027b7f8128b
4elodoy-Molovek <ivandor228@outlook.com>
2026-04-27 18:12:22 +0300

    lab3

---
+++ llvm/compiler-course/backend/dorofeev_i_backend_unroll/LoouUnroll.cpp
@@ -0,0 +1,94 @@
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineLoopInfo.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+struct DorofeevLoopUnroll : public MachineFunctionPass {
+  static char ID;
+  
+  const unsigned MaxIterations = 5; 
+
+  DorofeevLoopUnroll() : MachineFunctionPass(ID) {}
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<MachineLoopInfo>();
+    MachineFunctionPass::getAnalysisUsage(AU);
+  }
+
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    MachineLoopInfo &MLI = getAnalysis<MachineLoopInfo>();
+    bool Changed = false;
+
+    for (auto *L : MLI) {
+      Changed |= processLoop(L, MF);
+    }
+
+    return Changed;
+  }
+
+  bool processLoop(MachineLoop *L, MachineFunction &MF) {
+    bool Changed = false;
+
+    for (auto *SubL : L->getSubLoops()) {
+      Changed |= processLoop(SubL, MF);
+    }
+
+    if (L->getNumBlocks() != 1) {
+      return Changed;
+    }
+
+    MachineBasicBlock *MBB = L->getHeader();
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+    MachineRegisterInfo &MRI = MF.getRegInfo();
+
+    SmallVector<MachineInstr *, 8> BodyInstrs;
+    for (auto &MI : *MBB) {
+      if (!MI.isBranch() && !MI.isTerminator()) {
+        BodyInstrs.push_back(&MI);
+      }
+    }
+
+    for (unsigned i = 1; i < MaxIterations; ++i) {
+      DenseMap<Register, Register> RegMap;
+      
+      for (auto *MI : BodyInstrs) {
+        MachineInstrBuilder MIB = BuildMI(*MBB, MBB->getFirstTerminator(), 
+                                          MI->getDebugLoc(), TII->get(MI->getOpcode()));
+        
+        for (auto &MO : MI->operands()) {
+          if (MO.isReg()) {
+            Register Reg = MO.getReg();
+            if (Reg.isVirtual()) {
+              if (MO.isDef()) {
+                Register NewReg = MRI.createVirtualRegister(MRI.getRegClass(Reg));
+                RegMap[Reg] = NewReg;
+                MIB.addReg(NewReg, RegState::Define);
+              } else {
+                Register UseReg = RegMap.count(Reg) ? RegMap[Reg] : Reg;
+                MIB.addReg(UseReg);
+              }
+            } else {
+              MIB.add(MO); 
+            }
+          } else {
+            MIB.add(MO);
+          }
+        }
+      }
+      Changed = true;
+    }
+
+    return Changed;
+  }
+};
+} // namespace
+
+char DorofeevLoopUnroll::ID = 0;
+static RegisterPass<DorofeevLoopUnroll> X("dorofeev-loop-unroll", 
+                                          "Backend Loop Unroll Pass (Max 5 Iterations)", 
+                                          false, false);
